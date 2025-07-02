57e21ede00d92b52c494b4b23ee5d602725ce175
victornext <victorbrs517@gmail.com>
2025-07-01 23:20:46 -0300

    Subscriber para a fila de emissao de cartoes

--- src/main/java/com/io/github/victornext/msCardService/MsCardServiceApplication.java
+++ src/main/java/com/io/github/victornext/msCardService/MsCardServiceApplication.java
@@ -1,9 +1,11 @@
 package com.io.github.victornext.msCardService;
 
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableRabbit
 public class MsCardServiceApplication {
 
 	public static void main(String[] args) {
